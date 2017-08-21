5ddf96e00aaecb38ad563db0e35e661d99ed6db2
Maxime Guillemain <maxguillemain@gmail.com>
2017-08-21 18:08:49 +0200

    schema and gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170821145436) do
-  
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,12 +76,12 @@ ActiveRecord::Schema.define(version: 20170821145436) do
     t.string "avatar"
     t.text "bio"
     t.string "status"
-    t.string "photo"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
