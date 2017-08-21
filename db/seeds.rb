d1f329cc6f333c30932c1e9da92637e0f22dd3ce
sebastien milou <milouseba@gmail.com>
2017-08-21 19:17:13 +0200

    idees de texte a seeder dans db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,157 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# source textes : http://otheatre.over-blog.org/article-les-26-plus-celebres-monologues-classiques-115014490.html
+
+
+# propisition de texte exo + answer
+# 1 Inconnu **************************************************************************************
+# Quand j'arrive à la gare de l'Est, j'espère toujours secrètement qu'il y aura quelqu'un pour m'attendre.
+
+# C'est con. J'ai beau savoir que ma mère est encore au boulot à cette heure-là et que Marc
+# n'est pas du genre à traverser la banlieue pour porter mon sac, j'ai toujours cet espoir débile.
+# Là encore, ça n'a pas loupé, avant de descendre les escalators pour prendre le métro,
+# j'ai jeté un dernier regard circulaire au cas où il y aurait quelqu'un...
+# Et à chaque fois dans les escalators mon sac me paraît encore plus lourd.
+
+# Je voudrais que quelqu'un m'attende quelque part... C'est quand même pas compliqué.
+
+
+# 2 Dostoievski **************************************************************************************
+# "Je suis un homme malade...
+
+# Je suis un homme méchant. Un homme repoussoir.
+# Voilà ce que je suis. Je crois que j’ai quelque chose au foie.
+# De toute façon, ma maladie, je n’y comprends rien, j’ignore au juste ce qui me fait mal.
+# Je ne me soigne pas, je ne me suis jamais soigné, même si je respecte la médecine et les docteurs.
+# En plus, je suis superstitieux comme ce n’est pas permis: enfin, assez pour respecter la médecine.
+# (Je suis suffisamment instruit pour ne pas être superstitieux.) Oui, c’est par méchanceté que je ne me soigne pas.
+# Ça, messieurs, je parie que c’est une chose que vous ne comprenez pas.
+# Moi, si! Evidemment, je ne saurais vous expliquer à qui je fais une crasse quand j’obéis à ma méchanceté
+# de cette façon-là; je sais parfaitement que ce ne sont pas les docteurs que j’emmerde en refusant de me soigner;
+# je suis le mieux placé pour savoir que ça ne peut faire de tort qu’à moi seul et à personne d’autre.
+# Et, malgré tout, si je ne me soigne pas, c’est par méchanceté.
+
+# J’ai mal au foie. Tant mieux, qu’il me fasse encore mal!"
+
+
+# 3 Nietzsche **************************************************************************************
+# Mais comment pouvons-nous nous permettre de blâmer ou louer l’univers ?
+
+# Gardons-nous de lui reprocher de la dureté et de la raison, ou bien le contraire.
+# Il n’est ni parfait, ni beau, ni noble, et ne veut devenir rien de tout cela, il ne tend absolument pas à imiter l’homme !
+# Il n’est touché par aucun de nos jugements esthétiques et moraux ! Il ne possède pas non plus d’instinct de conservation et,
+# d’une façon générale, pas d’instinct du tout ; il ignore aussi toutes les lois.
+# Gardons-nous de dire qu’il y a des lois dans la nature.
+# Il n’y a que des nécessités : personne ne commande, personne n’obéit, personne ne désobéit.
+# Lorsque vous saurez qu’il n’y a point de fins,
+# vous saurez aussi qu’il n’y a point de hasard : car ce n’est qu’à côté d’un monde de fins que le mot « hasard » a un sens.
+# Gardons-nous de dire que la mort est opposé à la vie. La vie n’est qu’une variété de le mort et une variété très rare.
+# – Gardons-nous de penser que le monde crée éternellement du nouveau.
+# Il n’y a pas de substances éternellement durables ; la matière est une erreur pareille à celle des Dieux des Eléates.
+# Quand donc aurons-nous fini de nous ménager ? Quand toutes ces ombres de Dieu ne nous troublerons-elles plus ?
+# Quand aurons-nous entièrement dépouillé la nature de ses attributs divins ?
+# Quand retrouverons-nous la nature pure, innocente ?
+
+# Quand pourrons-nous, nous autres hommes, redevenir nature ?"
+
+
+# 4 "Battle Royale" Koushun Takami **************************************************************************************
+# Comment peut-on tuer ses camarades de classe ?
+
+# Certes, cette règle leur avait été imposée, mais Yumiko n'arrivait pas à croire qu'il existait quelqu'un
+# capable de jouer le jeu. Et pourtant...
+# Oui, et pourtant... Si elle se retrouvait face à quelqu'un qui en voulait à sa vie,
+# elle pouvait s'imaginer passer à l'acte, elle aussi. Et si elle en était capable, tout le monde le pouvait, non ?
+# Yumiko jeta un oeil sur un mégaphone apparemment abandonné dans un coin de la salle.
+# Etait-il en état de marche ? Et si oui...
+# S' il était utilisable, ne pouvait-elle le mettre à profit ?
+# La peur seule l'empêchait de s'en servir. Elle ne pouvait pas croire qu'il y ait des gens prêts à jouer le jeu
+# qui leur était imposé, mais d'un autre côté, elle ne pouvait pas nier qu'elle sentait son coeur pris
+# dans un filet d'angoisse.
+
+# C'est bien pour cette raison qu'avec Yukiko elles s'étaient éloignées de l'école et s'étaient enfermées ici.
+
+
+# 5 Jean Anouilh, ANTIGONE **************************************************************************************
+# Comprendre… Vous n’avez que ce mot-là à la bouche, tous, depuis que je suis toute petite.
+
+# Il fallait comprendre qu’on ne peut toucher à l’eau, à la belle eau fuyante parce que ça mouille les dalles,
+# à la terre parce que ça tache les robes. Il fallait comprendre qu’on ne doit pas manger tout à la fois,
+# donner tout ce qu’on a dans les poches au premier mendiant qu’on rencontre, courir,
+# courir dans le vent jusqu’à ce qu’on tombe par terre et boire quand on a chaud et se baigner quand il est trop tôt
+# ou trop tard, mais pas juste quand on en a envie !
+# Comprendre. Toujours comprendre..
+
+# Moi, je ne veux pas comprendre, Je comprendrai quand je serai vieille… Pas maintenant.
+
+
+# 6 Paolino (sous menace de mort) (Garçon) **************************************************************************************
+# Vous m’insultez ! Je suis un homme honnête, moi !
+
+# Je suis un homme de conscience, moi !
+# Je suis un homme, sachez-le, qui peut arriver à se trouver, bien sûr –sans le vouloir- dans une situation désespérée.
+# Oui ! Mais ce n’est pas vrai, pas vrai que je voudrais me servir des femmes des autres.
+# Parce que s’il en était ainsi, je ne vous aurais pas dit ce que je viens de vous dire, qu’un mari ne devrait
+# jamais négliger sa femme. Et j’ajoute maintenant qu’un mari qui néglige sa femme comment, selon moi, un crime !
+# Et pas rien qu’un ! Plusieurs crimes !
+# Oui parce que non seulement il oblige sa femme à manquer à ses devoirs en envers elle-même, envers son honnêteté,
+# mais parce qu’il peut aussi obliger un homme, un autre homme, à être malheureux toute sa vie.
+
+# Eh oui !
+
+
+# Choderlos de Laclos, Paul Achard, LES LIAISONS DANGEREUSES **************************************************************************************
+# Mais, on ne me dit rien, de sorte que, dans l’ignorance où je suis, je commets bien des gaucheries.
+
+# Mais oui : Ainsi hier, maman m’a dit de passer chez elle. Si c’était le monsieur, me dis-je !
+# La main me tremblait et le coeur me battait ; en entrant chez maman,
+# j’ai vu le monsieur en noir, debout près d’elle. Toute tremblante, j’ai trouvé un fauteuil et je me suis assise,
+# bien rouge et bien déconcertée. J’y étais à peine que voilà cet homme à mes genoux.
+# J’ai alors perdu la tête. Je me suis levée en jetant un cri perçant… comme lorsqu’il fait tonnerre.
+# Maman est partie d’un éclat de rire en me disant : « eh bien, qu’avez-vous ?
+# Donnez votre pied à monsieur… » Le monsieur était cordonnier.
+
+# Je ne peux pas vous rendre combien j’ai été honteuse.
+
+
+# Vaclav Havel, LARGO DESOLATO **************************************************************************************
+# C’est du baratin, tout ça. Quand tu voulais me séduire, au début, tu ne parlais pas comme ça.
+
+# J’allais faire renaître ton espoir, j’allais te régénérer, inaugurer pour toi une vie nouvelle.
+# Tu n’es pas une épave, Léopold, tu es un vulgaire démagogue.
+# Tu dirais n’importe quoi, tout ce qui t’arrange. Tu as eu tout ce que tu voulais et maintenant tu veux te débarrasser de moi.
+# Tu me parles de ton désarroi ! Foutaises, oui !
+# Tu veux me faire comprendre que j’ai rien à attendre de toi et en plus, tu veux te faire plaindre.
+# C’est malhonnête. Ce sont des grands mots, mais tu ne m’auras pas comme ça.
+# Oh !, comme j’ai été bête, bête à pleurer. Croire que je pourrais te faire partager mes sentiments,
+# te redonner goût à la vie ? Tu parles !
+
+# Tu es un cas désespéré. C’est bien fait pour moi. Une illusion de moins.
+
+# Vaclav Havel, LARGO DESOLATO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
