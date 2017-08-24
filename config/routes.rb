7fda20d7b86ecc974f89d5c12589c2c024ae2db2
sebastien milou <milouseba@gmail.com>
2017-08-24 11:53:24 +0200

    redirect vers index when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+
+    root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :exercices, only: [:index, :show, :new, :create] do
     resources :answers, only: [:create]
