ff9b2b50262607afd52fcfe156efcc820f781c0d
sebastien milou <milouseba@gmail.com>
2017-08-24 12:58:07 +0200

    test de longueur des paragraphes

---
+++ testcodefile.rb
@@ -0,0 +1,18 @@
+deb = "Les arbres derrière eux fumaient encore ; de leurs branches noircies, des carcasses de singes à demi brûlées
+tombaient de temps à autre au milieu des plats. Les soldats ivres ronflaient la bouche ouverte à côté des cadavres ;
+et ceux qui ne dormaient pas baissaient leur tête, éblouis par le jour. Le sol piétiné disparaissait sous des flaques
+rouges."
+
+p deb.split(" ").length
+
+mid = "Lorient est créée en 1666 dans un domaine appelé « l'Enclos » et se développe en dehors de celui-ci.
+Kerentrech, Merville, La Perrière, Calvin et Keryado constituent les faubourgs de la ville.
+L'ensemble est clos par des murailles érigées en 1744. La ville s'étend en absorbant le faubourg de Kerentrech en 1791
+et celui de Merville en 1808. L'extension à l'ouest du Faouëdic est décidée entre 1857 et 1861 et
+le quartier de la Nouvelle-Ville est construit à partir de 1873 qui double la superficie de la ville.
+Les remparts et les portes du Morbihan et de Ploemeur sont détruites en 1906.
+Lorient annexe, par arrêté préfectoral du 10 septembre 1947, la petite commune voisine de Keryado qui
+fut instituée en commune propre le 18 avril 1901 lors de sa scission de celle de Ploemeur à laquelle elle
+était rattachée comme section depuis 1789 et auparavant était incluse dans sa paroisse"
+p mid.split(" ").length
+
