56efc051da7ec2dd330a8c72960443a8842bad22
sebastien milou <milouseba@gmail.com>
2017-08-25 11:49:28 +0200

    js agrandit/retreci tetxtbox onclick

--- app/assets/javascripts/showCountWords.js
+++ app/assets/javascripts/showCountWords.js
@@ -10,24 +10,12 @@ document.getElementById('answer_content').addEventListener('keyup', (event) => {
   // document.getElementById('btn-publish').className.replace("link-visible", "link-hidden");
 });
 
-// document.getElementById('answer_content').addEventListener('click', (event) => {
+document.getElementById('answer_content').addEventListener('click', (event) => {
+  event.target.classList.add("answer-content-grow");
+});
 
-//   let count = 0;
-//   event.target.value.split(" ").forEach(val => {
-//     if (val !== "") {
-//       count++;
-//     }
-//   });
-//   document.getElementById('words-count').innerText = count;
-
-// });
-
-// document.getElementById('btn-publish').addEventListener('keyup', (event) => {
-
-
-
-// }
-
-// <a class="btn-publish link-hidden" hidden="hidden" href="/exercices/236/publish">Publier</a>
-
-// tablinks[i].className.replace(" active", "");
+document.addEventListener('click', (event) => {
+  if (event.target.className.indexOf("answer-content-grow") === -1) {
+    document.getElementById('answer_content').classList.remove("answer-content-grow");
+  }
+});
