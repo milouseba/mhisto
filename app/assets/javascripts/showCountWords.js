0c6c2fd5439f714f27703dfe199c1d17bd9e99ca
sebastien milou <milouseba@gmail.com>
2017-08-25 14:46:11 +0200

    test sans js heroku

--- app/assets/javascripts/showCountWords.js
+++ app/assets/javascripts/showCountWords.js
@@ -1,21 +1,21 @@
-document.getElementById('answer_content').addEventListener('keyup', (event) => {
+// document.getElementById('answer_content').addEventListener('keyup', (event) => {
 
-  let count = 0;
-  event.target.value.split(" ").forEach(val => {
-    if (val !== "") {
-      count++;
-    }
-  });
-  document.getElementById('words-count').innerText = count;
-  // document.getElementById('btn-publish').className.replace("link-visible", "link-hidden");
-});
+//   let count = 0;
+//   event.target.value.split(" ").forEach(val => {
+//     if (val !== "") {
+//       count++;
+//     }
+//   });
+//   document.getElementById('words-count').innerText = count;
+//   // document.getElementById('btn-publish').className.replace("link-visible", "link-hidden");
+// });
 
-document.getElementById('answer_content').addEventListener('click', (event) => {
-  event.target.classList.add("answer-content-grow");
-});
+// document.getElementById('answer_content').addEventListener('click', (event) => {
+//   event.target.classList.add("answer-content-grow");
+// });
 
-document.addEventListener('click', (event) => {
-  if (event.target.className.indexOf("answer-content-grow") === -1) {
-    document.getElementById('answer_content').classList.remove("answer-content-grow");
-  }
-});
+// document.addEventListener('click', (event) => {
+//   if (event.target.className.indexOf("answer-content-grow") === -1) {
+//     document.getElementById('answer_content').classList.remove("answer-content-grow");
+//   }
+// });
