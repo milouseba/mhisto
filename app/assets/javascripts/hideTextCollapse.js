0fdf73002939fcfe424ad89cac81ca04010f3081
Maxime Guillemain <maxguillemain@gmail.com>
2017-08-31 12:47:59 +0200

    collapse text and change design show

--- app/assets/javascripts/hideTextCollapse.js
+++ app/assets/javascripts/hideTextCollapse.js
@@ -2,12 +2,11 @@
 $(".show-collapse").click(function() {
   $(this).hide();
   $(this).parent().find(".hide-collapse").show();
-  console.log('fhbej');
 });
-$( ".hide-collapse" ).click(function() {
-  $(this).hide();
-  $(this).parent().find(".show-collapse").show();
-  console.log('fhbej');
+
+$( ".fa-minus" ).click(function() {
+  $(".hide-collapse").hide();
+  $(this).parents(".answer").find(".show-collapse").show();
 });
 
 
