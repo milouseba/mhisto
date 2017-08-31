7a20945a07e02feeb1408dec67687401b516cb38
Thomas Chekaiban <thomaschekaiban@gmail.com>
2017-08-31 13:54:48 +0200

    done

--- app/assets/javascripts/scrollNavbar.js
+++ app/assets/javascripts/scrollNavbar.js
@@ -1,39 +1,39 @@
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $('header').outerHeight();
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
 
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//     }
-// }, 250);
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
 
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
+function hasScrolled() {
+    var st = $(this).scrollTop();
 
-//     // Make sure they scroll more than delta
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
 
-//     // If they scrolled down and are past the navbar, add class .nav-up.
-//     // This is necessary so you never see what is "behind" the navbar.
-//     if (st > lastScrollTop && st > navbarHeight){
-//         // Scroll Down
-//         $('header').removeClass('navbar-wagon').addClass('nav-up');
-//         $('.dropdown').addClass('dropdown-new');
-//     } else {
-//         // Scroll Up
-//         if(st + $(window).height() < $(document).height()) {
-//             $('header').removeClass('nav-up').addClass('navbar-wagon');
-//             $('.dropdown').removeClass('dropdown-new');
-//         }
-//     }
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('navbar-wagon').addClass('nav-up');
+        $('.dropdown').addClass('dropdown-new');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('navbar-wagon');
+            $('.dropdown').removeClass('dropdown-new');
+        }
+    }
 
-//     lastScrollTop = st;
-// }
+    lastScrollTop = st;
+}
